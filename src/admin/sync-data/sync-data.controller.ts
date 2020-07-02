06a0d1381e2d2e75e8283688b8463c8fe445bccd
Fakhri <fakhrialuwe@rocketmail.com>
2020-07-03 00:24:24 +0800

    update sync api

--- src/admin/sync-data/sync-data.controller.ts
+++ src/admin/sync-data/sync-data.controller.ts
@@ -42,7 +42,7 @@ export class SyncDataController {
   @Post('testsyncsubscription')
   @ApiOperation({ title: 'Test sync' })
   testSyncSubscription(@Req() req, @Res() res) {
-    console.log(req.headers);
+    console.log(req);
     const urlSubscription = `https://beesuite.app:3003/subscription`;
     this.syncdataService.customerDbService.httpService.get(urlSubscription).subscribe(
       data => { res.status(HttpStatus.OK).send(data.data); },
