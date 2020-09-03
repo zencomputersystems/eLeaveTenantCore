b7b702fff5015b5af862ccc066a8de850a8dec5c
Fakhri <fakhrialuwe@rocketmail.com>
2020-09-03 10:10:27 +0800

    remove attachment for suggestions

--- src/admin/support/support.service.ts
+++ src/admin/support/support.service.ts
@@ -129,6 +129,7 @@ export class SupportService {
                 delete x.START_TIME;
                 delete x.END_TIME;
                 delete x.STATUS;
+                delete x.ATTACHMENT;
               }
             });
             let requestData = res.filter(x => x.REQUEST_TYPE != 'suggestions');
