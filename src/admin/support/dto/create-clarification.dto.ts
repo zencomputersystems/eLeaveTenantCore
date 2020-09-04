c98773fe0cc43830241435c78e82195aac21ec70
Fakhri <fakhrialuwe@rocketmail.com>
2020-09-04 16:00:38 +0800

    change attachment from array to string

--- src/admin/support/dto/create-clarification.dto.ts
+++ src/admin/support/dto/create-clarification.dto.ts
@@ -8,9 +8,9 @@ export class CreateClarificationDTO {
   @ApiModelProperty({ description: 'User id', example: '697b25ac-bff1-b1d1-f17e-fa0206fc7a2a' })
   @IsString()
   userId: string;
-  @ApiModelProperty({ description: 'Supporting document', example: ["779419_firstfile.png", "6987604_secondfile.jpg"] })
-  @IsArray()
-  doc: string[];
+  @ApiModelProperty({ description: 'Supporting document', example: "6987604_secondfile.jpg" })
+  @IsString()
+  doc: string;
   @ApiModelProperty({ description: 'Message', example: 'Dear all,\n\n  In accordance with Hari Raya celebration, we would like to request all staff to take 1 day annual leave due to a close down of operations on 7 June 2019. Kindly apply annual leave for the low productivity period and take this as an opportunity to have a substantial break for family. \n  \n  This is not applicable to Manage365 and backup of Resident Engineer.\n' })
   @IsString()
   message: string;
